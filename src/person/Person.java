dfbf06f266f646735803a8992cdf6957da562874
Gaurav Singh <gaurav.singh_cs18@gla.ac.in>
2020-02-24 22:50:52 +0530

    fields created : firstName, lastName, contacts and email

--- src/person/Person.java
+++ src/person/Person.java
@@ -1,4 +1,10 @@
 package person;
 
+import java.util.LinkedList;
+
 public class Person {
+    private String firstName;
+    private String lastName;
+    private LinkedList<String> contacts;
+    private String email;
 }
